b87c65b963112000d3eca533c483b24bf388bd49
Murillo Albano <student25@jala-foundation.org>
2022-08-27 13:16:41 -0300

    button and click button function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 let display =  document.querySelector('.display')
-let name =  document.querySelector('.name')
+let id =  document.querySelector('.id')
 let addedDate =  document.querySelector('.addedDate')
 let tags =  document.querySelector('.tags')
+
+
+fetch('https://cataas.com/api/cats')
+.then(response  => response.json())
+.then(data => {
+    let id = data['id']
+    let addedDate = data['created_at']
+    let tags  = data['tags']
+        
+    id.innerHTML = id
+    addedDate.innerHTML = addedDate
+    tags.innerHTML = tags
+})
+  
+  
+  document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
+  function showDiv() {
+    document.getElementById('show-hide').style.display = "block";
+ }
 
