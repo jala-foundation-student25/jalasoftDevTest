aa7dcec28f7cc107f74a606a46e292fc5b5b2658
Murillo Albano <student25@jala-foundation.org>
2022-08-27 11:20:24 -0300

    Added the gif url to the link page

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let display =  document.querySelector('.display')
+let name =  document.querySelector('.name')
+let addedDate =  document.querySelector('.addedDate')
+let tags =  document.querySelector('.tags')
+
